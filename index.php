4248adac47644f0a017225111173df8e1e56b0cc
Agustin <agustin.pedrote@iesdonana.org>
2022-10-14 15:24:46 +0200

    busqueda por rangos

--- index.php
+++ index.php
@@ -1,37 +1,79 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Departamentos</title>
 </head>
+
 <body>
-    <!-- psql -h localhost -d empresa -U empresa
-         psql -h localhost -d empresa -U empresa < empresa.sql
-    -->
-    
+    <?php
+    $desde_codigo = (isset($_GET['desde_codigo'])) ? trim($_GET['desde_codigo']) : null;
+    $hasta_codigo = (isset($_GET['hasta_codigo'])) ? trim($_GET['hasta_codigo']) : null;
+    ?>
+
+    <div>
+        <form action="" method="get">
+            <fieldset>
+                <legend>Criterios de búsqueda</legend>
+                <p>
+                    <label>
+                        Desde código:
+                        <input type="text" name="desde_codigo" size="8" value="<?= $desde_codigo ?>">
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        Hasta código:
+                        <input type="text" name="hasta_codigo" size="8" value="<?= $hasta_codigo ?>">
+                    </label>
+                </p>
+                <button type="submit">Buscar</button>
+            </fieldset>
+        </form>
+    </div>
     <?php
     $pdo = new PDO('pgsql:host=localhost;dbname=empresa', 'empresa', 'empresa');
-    $sent = $pdo->query('SELECT * FROM departamentos ORDER BY codigo');
-    $filas = $sent->fetchAll();?>
-
-    <table border="1"><?php
-    foreach ($filas as $key => $value) {?>
-    <tr>
-        <td><?php print_r($value['codigo']); ?></td>
-        <td><?php print_r($value['denominacion']); ?></td>
-    </tr>
-    <?php
-    }?>
-    </table>
-    <?php
-
-/*     echo "<pre>";
-    foreach ($sent as $fila) {
-        print_r($fila);
-    }
-    echo "</pre>"; */
+    $pdo->beginTransaction();
+    $sent = $pdo->query('LOCK TABLE departamentos IN SHARE MODE');
+    $sent = $pdo->prepare('SELECT COUNT(*) 
+                             FROM departamentos 
+                            WHERE codigo BETWEEN :desde_codigo AND :hasta_codigo');
+   $sent->execute([
+        ':desde_codigo' => $desde_codigo,
+        ':hasta_codigo' => $hasta_codigo,
+    ]);
+    $total = $sent->fetchColumn();
+    $sent = $pdo->prepare('SELECT *
+                             FROM departamentos 
+                            WHERE codigo BETWEEN :desde_codigo AND :hasta_codigo
+                         ORDER BY codigo');
+    $sent->execute([
+        ':desde_codigo' => $desde_codigo,
+        ':hasta_codigo' => $hasta_codigo,
+    ]);
+    $pdo->commit();
     ?>
+    <br>
+    <div>
+        <table style="margin: auto" border="1">
+            <thead>
+                <th>Código</th>
+                <th>Denominación</th>
+            </thead>
+            <tbody>
+                <?php foreach ($sent as $fila) : ?>
+                    <tr>
+                        <td><?= $fila['codigo'] ?></td>
+                        <td><?= $fila['denominacion'] ?></td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+        <p>Número total de filas: <?= $total ?> </p>
+    </div>
 </body>
+
 </html>
